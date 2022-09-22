fa3521763c8c52023c214751dbb40d93e71dca09
Alexander A Cerlini <contato@alexcerlini.com.br>
2022-09-22 13:47:19 -0300

    feat: add linkedin button to home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,9 @@ import React from "react";
 
 import { Container } from "./styles";
 import Header from "../../components/Header";
+import Button from "../../components/Button";
+
+import LinkedinIcon from "../../assets/icons/linkedin.svg";
 
 const Home: React.FC = () => {
   return (
@@ -11,6 +14,11 @@ const Home: React.FC = () => {
       <Header
         title="Alexander Andrade Cerlini"
         subtitle="Front-end Developer"
+      />
+      <Button
+        icon={LinkedinIcon}
+        title="Linkedin"
+        url="https://www.linkedin.com/in/alex-cerlini/"
       />
     </Container>
   );
