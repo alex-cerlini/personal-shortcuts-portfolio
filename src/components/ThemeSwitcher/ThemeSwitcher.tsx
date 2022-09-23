4e048c3ae4890ef7156b76dfa68b28355d5f123e
Alexander A Cerlini <contato@alexcerlini.com.br>
2022-09-23 17:40:38 -0300

    fix: moon change to white on dark mode

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 
 import MoonIcon from "../../assets/icons/moon.svg";
+import MoonDarkIcon from "../../assets/icons/moonDarkMode.svg";
 
 import { Container, IconContainer } from "./styles";
 
+import useThemeStore from "../../stores/themeStore";
+
 const ThemeSwitcher: React.FC = () => {
+  const lightTheme = useThemeStore((state) => state.lightTheme);
   return (
     <Container>
-      <IconContainer src={MoonIcon} alt="icone de temas" />
+      <IconContainer
+        src={lightTheme ? MoonIcon : MoonDarkIcon}
+        alt="icone de temas"
+      />
     </Container>
   );
 };
